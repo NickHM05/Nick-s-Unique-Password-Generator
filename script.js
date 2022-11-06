0cb2ff08698903586bdf9fda9d20fcb89b645f7c
NicholasHM05 <nhmamberger@gmail.com>
2022-11-06 16:45:15 -0600

    added window prompt for how long password will be.

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
 
-  var userInput = window.prompt("How long will your password be?")
 
-  var passwordsLength = parseInt(userInput)
+function generatePassword() {
+  
+   var userInput = window.prompt("How long will your password be?")
 
-/*NaN stands for is not a number, so if the input from the user is not a number that is between 8 and 128 */ 
-  if (isNaN(passwordsLength)) {
+   var passwordLength = parseInt(userInput)
+
+ /*NaN stands for is not a number. */ 
+  if (isNaN(passwordLength)) {
     window.alert("That's not a number!")
     return
-  }
+ }
 
-if (passwordsLength < 8 || passwordsLength > 128) {
-  window.alert("Password character length must be between 8 and 128 characters in order to continue")
-  return
+
 }
 
-// Add event listener to generate button. 
-// 53;34
-/*var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-*/
-generateBtn.addEventListener("click", writePassword);
-
-if (passwordsLength < 8 || passwordsLength > 128) {
-  window.alert("Password character length must be between 8 and 128 characters in order to continue")
-  return
-}
-}
+// Write password to the #password input
+function writePassword() {
+   var password = generatePassword();
+   var passwordText = document.querySelector("#password");
+ 
+   passwordText.value = password;
+ 
+ }
+ 
+ // Add event listener to generate button
+ generateBtn.addEventListener("click", writePassword);

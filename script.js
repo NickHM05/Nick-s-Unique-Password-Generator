2cce8dc9d550724a23dd4fcecf633d92b709bdb6
NicholasHM05 <nhmamberger@gmail.com>
2022-11-06 15:02:51 -0600

    reverting code to try to fix js from not linking to index

--- script.js
+++ script.js
@@ -3,17 +3,33 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+  var userInput = window.prompt("How long will your password be?")
+
+  var passwordsLength = parseInt(userInput)
+
+/*NaN stands for is not a number, so if the input from the user is not a number that is between 8 and 128 */ 
+  if (isNaN(passwordsLength)) {
+    window.alert("That's not a number!")
+    return
+  }
+
+if (passwordsLength < 8 || passwordsLength > 128) {
+  window.alert("Password character length must be between 8 and 128 characters in order to continue")
+  return
+}
+
+// Add event listener to generate button. 
+// 53;34
+/*var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-}
-
-// Add event listener to generate button
+*/
 generateBtn.addEventListener("click", writePassword);
 
-if(8 <= password <= 128) {
-  console.log("Password set, include capital letters/symbols?");
+if (passwordsLength < 8 || passwordsLength > 128) {
+  window.alert("Password character length must be between 8 and 128 characters in order to continue")
+  return
 }
-else (generate password)
+}

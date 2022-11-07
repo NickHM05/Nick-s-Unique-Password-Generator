447a999b9b7787235c8ebe359db6d8b592421788
NicholasHM05 <nhmamberger@gmail.com>
2022-11-06 18:32:56 -0600

    added info to my read me for a great layout. also added a return code piece so that the user can return if no password was made

--- script.js
+++ script.js
@@ -90,8 +90,13 @@ function writePassword() {
    var password = generatePassword();
    var passwordText = document.querySelector("#password");
  
+   if (!password)return
+
+   if(password) {
    passwordText.value = password;
- 
+   }
+
+   
  }
  
  // Add event listener to generate button
